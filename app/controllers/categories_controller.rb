b8d09d4dafff65fcd70924b6cf302968f9cd4ce2
Thang Ruby <giathang.huflit@gmail.com>
2015-12-17 09:02:27 +0700

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -68,39 +68,7 @@ class CategoriesController < ApplicationController
       format.json { head :no_content }
     end
   end
-   #create crawl
-  def crawl
-    @categories = Category.all
-    @categories.each do |category|
-      articles_links = Nokogiri::HTML(open('http://9gag.com/'+ category.name))
-      articles_links.xpath('//article/@data-entry-url').each do |link|
-        article = Nokogiri::HTML(open(link))
-        article_id = (link.text).rpartition('/').last
-
-        title = article.xpath('//header/h2').text
-        
-        point_of_article ="a#love-count-" + article_id + "span span.badge-item-love-count"
-        point = article.css(point_of_article).text
-
-        idx = 0 
-        image_url = article.xpath('//img').first['src']
-
-        post = Post.create(category_id: category.id,title: title,image_url: image_url,point: point)
-
-        comment_url = "http://d8d94e0wul1nb.cloudfront.net/comment?callback=comment_list&appId=a_dd8f2b7d304a10edaf6f29517ea0ca4100a43d1b&url=http%3A%2F%2F9gag.com%2Fgag%2F" + article_id + "&order=score&count=10&level=2&ref=&refCommentId=&commentId="
-        result = Net::HTTP.get(URI.parse(comment_url))
-        comment_json_data = result[13..result.length-3]
-        comment_data = JSON.parse(comment_json_data)
-        comment_list = comment_data["payload"]["comment"]
-        comment_list.each do |comment|
-          name = comment["commentId"]
-          body = comment["text"]
-          post_id = post.id
-          Comment.create(post_id: post_id,name: name,body: body)
-        end
-      end
-    end
-  end
+   
 
   def generate_json
     @category = Category.find(params[:id])
@@ -129,6 +97,6 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name, :is_crawl)
+      params.require(:category).permit(:name)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -9,7 +9,6 @@ class Category < ActiveRecord::Base
 	def self.crawl
 		@categories = Category.all
 		@categories.each do |category|
-			if category.is_crawl == "true"
 				articles_links = Nokogiri::HTML(open('http://9gag.com/' + category.name))
 				articles_links.xpath('//article/@data-entry-url').each do |link|
 					article = Nokogiri::HTML(open(link))
@@ -20,11 +19,10 @@ class Category < ActiveRecord::Base
 					point_of_article = "a#love-count-" + article_id + "span span.badge-item-love-count"
 					point = article.css(point_of_article).text
 
-					idx=0
 					image_url = article.xpath('//img').first['src']
 					post = Post.create(category_id: category.id,title: title,image_url: image_url,point: point)
 
-					comment_url = "http://d8d94e0wul1nb.cloudfront.net/comment?callback=comment_list&appId=a_dd8f2b7d304a10edaf6f29517ea0ca4100a43d1b&url=http%3A%2F%2F9gag.com%2Fgag%2F" + article_id + "&order=score&count=10&level=2&ref=&refCommentId=&commentId="  
+					comment_url = "http://9gag.com/gag/" + article_id + "#comment"  
       		result = Net::HTTP.get(URI.parse(comment_url))
        		comment_json_data = result[13..result.length-3]
 
@@ -40,6 +38,6 @@ class Category < ActiveRecord::Base
 		      end    
 				end
 			end
-		end
+		
 	end
 end
